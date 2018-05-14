6e1ac296adb248d916d506447823e63963f9ac1f
CleptomaniacDwarf <melina.kroenert@web.de>
2018-05-14 12:48:57 +0200

    Sicherung

--- src/index.js
+++ src/index.js
@@ -1,39 +1,38 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Rnd from 'react-rnd';
+import React from "react";
+import { render } from "react-dom";
+import Rnd from "react-rnd";
 
 const style = {
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center"
 };
 
 class App extends React.Component {
-
   constructor() {
     super();
     this.state = {
-      width1: 200,
-      height1: 200,
+      width1: 320,
+      height1: 720,
       x1: 10,
       y1: 10,
-      width2: 200,
-      height2: 200,
-      x2: 10,
+      width2: 320,
+      height2: 720,
+      x2: 330,
       y2: 10,
-      width3: 200,
-      height3: 200,
-      x3: 10,
+      width3: 320,
+      height3: 720,
+      x3: 650,
       y3: 10,
-      width4: 200,
-      height4: 200,
-      x4: 10,
-      y4: 10,
-    }
+      width4: 320,
+      height4: 720,
+      x4: 970,
+      y4: 10
+    };
   }
 
   render() {
-      return (
+    return (
       <div>
         <Rnd
           style={style}
@@ -47,27 +46,31 @@ class App extends React.Component {
             this.setState({
               width: ref.offsetWidth,
               height: ref.offsetHeight,
-              ...position,
+              x2: this.state.x2 + ref.offsetWidth,
+              x3: this.state.x3 + ref.offsetWidth,
+              x4: this.state.x4 + ref.offsetWidth,
             });
           }}
         >
-          <div style={{
-            //width: '300px',
-            height: '720px',
-            overflow: 'hidden',
-          }}>
-            <video autoPlay
+          <div
+            style={{
+              width: this.state.width1,
+              height: "720px",
+              overflow: "hidden"
+            }}
+          >
+            <video
+              autoPlay
               style={{
-                objectFit: 'cover',
-                width: '100%',
-                height: '100%',
-              }}>
-              <source
-                src="https://webuser.hs-furtwangen.de/~kroenert/AudioVideo/Melody.mp4" />
+                objectFit: "cover",
+                width: "100%",
+                height: "100%"
+              }}
+            >
+              <source src="https://webuser.hs-furtwangen.de/~kroenert/AudioVideo/Melody.mp4" />
               />
-    </video>
+            </video>
           </div>
-
         </Rnd>
         <Rnd
           style={style}
@@ -75,33 +78,35 @@ class App extends React.Component {
             right: true
           }}
           size={{ width: this.state.width2, height: this.state.height2 }}
-          position={{ x: 210, y: this.state.y2 }}
+          position={{ x: this.state.x2, y: this.state.y2 }}
           disableDragging
           onResize={(e, direction, ref, delta, position) => {
             this.setState({
               width: ref.offsetWidth,
               height: ref.offsetHeight,
-              ...position,
+              ...position
             });
           }}
         >
-          <div style={{
-            //width: '300px',
-            height: '720px',
-            overflow: 'hidden',
-          }}>
-            <video autoPlay
+          <div
+            style={{
+              //width: '300px',
+              height: "720px",
+              overflow: "hidden"
+            }}
+          >
+            <video
+              autoPlay
               style={{
-                objectFit: 'cover',
-                width: '100%',
-                height: '100%',
-              }}>
-              <source
-                  src="https://webuser.hs-furtwangen.de/~kroenert/AudioVideo/Bass.mp4" />
+                objectFit: "cover",
+                width: "100%",
+                height: "100%"
+              }}
+            >
+              <source src="https://webuser.hs-furtwangen.de/~kroenert/AudioVideo/Bass.mp4" />
               />
-    </video>
+            </video>
           </div>
-
         </Rnd>
         <Rnd
           style={style}
@@ -109,33 +114,35 @@ class App extends React.Component {
             right: true
           }}
           size={{ width: this.state.width3, height: this.state.height3 }}
-          position={{ x: 410, y: this.state.y3 }}
+          position={{ x: this.state.x3, y: this.state.y3 }}
           disableDragging
           onResize={(e, direction, ref, delta, position) => {
             this.setState({
               width: ref.offsetWidth,
               height: ref.offsetHeight,
-              ...position,
+              ...position
             });
           }}
         >
-          <div style={{
-            //width: '300px',
-            height: '720px',
-            overflow: 'hidden',
-          }}>
-            <video autoPlay
+          <div
+            style={{
+              //width: '300px',
+              height: "720px",
+              overflow: "hidden"
+            }}
+          >
+            <video
+              autoPlay
               style={{
-                objectFit: 'cover',
-                width: '100%',
-                height: '100%',
-              }}>
-              <source
-                  src="https://webuser.hs-furtwangen.de/~kroenert/AudioVideo/Drums.mp4" />
+                objectFit: "cover",
+                width: "100%",
+                height: "100%"
+              }}
+            >
+              <source src="https://webuser.hs-furtwangen.de/~kroenert/AudioVideo/Drums.mp4" />
               />
-    </video>
+            </video>
           </div>
-
         </Rnd>
 
         <Rnd
@@ -144,37 +151,39 @@ class App extends React.Component {
             right: true
           }}
           size={{ width: this.state.width4, height: this.state.height4 }}
-          position={{ x: 610, y: this.state.y4 }}
+          position={{ x: this.state.x4, y: this.state.y4 }}
           disableDragging
           onResize={(e, direction, ref, delta, position) => {
             this.setState({
               width: ref.offsetWidth,
               height: ref.offsetHeight,
-              ...position,
+              ...position
             });
           }}
         >
-          <div style={{
-            //width: '300px',
-            height: '720px',
-            overflow: 'hidden',
-          }}>
-            <video autoPlay
+          <div
+            style={{
+              //width: '300px',
+              height: "720px",
+              overflow: "hidden"
+            }}
+          >
+            <video
+              autoPlay
               style={{
-                objectFit: 'cover',
-                width: '100%',
-                height: '100%',
-              }}>
-              <source
-                  src="https://webuser.hs-furtwangen.de/~kroenert/AudioVideo/Vox.mp4" />
+                objectFit: "cover",
+                width: "100%",
+                height: "100%"
+              }}
+            >
+              <source src="https://webuser.hs-furtwangen.de/~kroenert/AudioVideo/Vox.mp4" />
               />
-    </video>
+            </video>
           </div>
-
         </Rnd>
       </div>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
