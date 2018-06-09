e60d67d384f999a442a59d6cd97597246a061690
CleptomaniacDwarf <melina.kroenert@web.de>
2018-06-09 17:37:44 +0200

    Grosse Aenderung

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import Rnd from "react-rnd";
+import ReactPlayer from "react-player";
 
 const style = {
   display: "flex",
@@ -33,10 +34,89 @@ class App extends React.Component {
       proportion2: 1,
       proportion3: 1,
       proportion4: 1,
-      };
-    }
+      player1Ready: false,
+      player2Ready: false,
+      player3Ready: false,
+      player4Ready: false,
+      player1Mute: false,
+      player2Mute: false,
+      player3Mute: false,
+      player4Mute: false,
+      player1: React.createRef(),
+      player2: React.createRef(),
+      player3: React.createRef(),
+      player4: React.createRef(),
+      start: false,
+      initialPlay: true
+    };
+  }
 
-  checkWidth(identity) {
+  player1Ready = () => {
+    this.setState({
+      player1Ready: true
+    });
+  };
+
+  player2Ready = () => {
+    this.setState({
+      player2Ready: true
+    });
+  };
+
+  player3Ready = () => {
+    this.setState({
+      player3Ready: true
+    });
+  };
+
+  player4Ready = () => {
+    this.setState({
+      player4Ready: true
+    });
+  };
+
+  player1Mute = () => {
+    this.setState({
+      player1Mute: !this.state.player1Mute
+    });
+  };
+
+  player2Mute = () => {
+    this.setState({
+      player2Mute: !this.state.player2Mute
+    });
+  };
+
+  player3Mute = () => {
+    this.setState({
+      player3Mute: !this.state.player3Mute
+    });
+  };
+
+  player4Mute = () => {
+    this.setState({
+      player4Mute: !this.state.player4Mute
+    });
+  };
+
+  player1Started = () => {
+    console.log(`Player 1 started at ${Date.now()}`);
+  };
+
+  player2Started = () => {
+    console.log(`Player 2 started at ${Date.now()}`);
+  };
+
+  player3Started = () => {
+    console.log(`Player 3 started at ${Date.now()}`);
+  };
+
+  player4Started = () => {
+    console.log(`Player 4 started at ${Date.now()}`);
+  };
+
+
+  /*checkWidth(identity) {
     if (identity == "melody") {
       this.state.total = this.state.width2 + this.state.width3 + this.state.width4;
       this.state.proportion2 = this.state.width2 / this.state.total;
@@ -74,8 +154,40 @@ class App extends React.Component {
       this.state.width3 = this.state.width3 * this.state.proportion4;
     }
   }
+  */
 
   render() {
+    if (
+      this.state.player1Ready &&
+      this.state.player2Ready &&
+      this.state.player3Ready &&
+      this.state.player4Ready &&
+      !this.state.start
+    ) {
+      console.log("Prebuffering");
+      setTimeout(() => this.setState({ start: true }), 5000);
+    }
+
+    if (
+      this.state.player1Ready &&
+      this.state.player2Ready &&
+      this.state.player3Ready &&
+      this.state.player4Ready &&
+      this.state.start &&
+      this.state.initialPlay
+    ) {
+      console.log("All videos are playing");
+
+      setTimeout(() => this.setState({ initialPlay: false }), 5000);
+
+      setTimeout(() => {
+        this.state.player1.current.seekTo(0);
+        this.state.player2.current.seekTo(0);
+        this.state.player3.current.seekTo(0);
+        this.state.player4.current.seekTo(0);
+      }, 100);
+    }
+    
     return (
       <div>
         <Rnd
@@ -105,14 +217,14 @@ class App extends React.Component {
               x2: x2,
               x3: x3,
               x4: x4,
-              //Breitenberechnung
-              total: 1280 - ref.offsetWidth,
-              proportion2: this.state.width2 / this.state.total,
-              proportion3: this.state.width3 / this.state.total,
-              proportion4: this.state.width4 / this.state.total,
-              width2: this.state.width2 * this.state.proportion2,
-              width3: this.state.width3 * this.state.proportion3,
-              width4: this.state.width4 * this.state.proportion4,
+             //Breitenberechnung
+              //total: 1280 - ref.offsetWidth,
+              //proportion2: this.state.width2 / this.state.total,
+              //proportion3: this.state.width3 / this.state.total,
+              //proportion4: this.state.width4 / this.state.total,
+              //width2: this.state.width2 * this.state.proportion2,
+              //width3: this.state.width3 * this.state.proportion3,
+              //width4: this.state.width4 * this.state.proportion4, --!>
              });
             //checkWidth(e.id);
           }}
@@ -123,18 +235,34 @@ class App extends React.Component {
               height: "720px",
               overflow: "hidden",
             }}
+            onClick={this.player1Mute}
           >
-            <video
-              autoPlay
-              style={{
-                objectFit: "cover",
-                width: "100%",
-                height: "100%",
+            <ReactPlayer
+              playing={
+                this.state.player1Ready &&
+                this.state.player2Ready &&
+                this.state.player3Ready &&
+                this.state.player4Ready &&
+                this.state.start
+              }
+              ref={this.state.player1}
+              config={{
+                file: {
+                  attributes: {
+                    preload: "auto",
+                    onCanPlayThrough: this.player1Ready
+                  }
+                }
               }}
-            >
-              <source src="https://webuser.hs-furtwangen.de/~kroenert/AudioVideo/Melody.mp4" />
-              />
-            </video>
+              volume={this.state.width1 / 640}
+              muted={this.state.player1Mute}
+              onStart={this.player1Started}
+              width="auto"
+              height="auto"
+              url={
+                "https://sftp.hs-furtwangen.de/~kroenert/AudioVideo/VeralteteVideos/Melody.mp4"
+              }
+            />
           </div>
         </Rnd>
         <Rnd
@@ -160,13 +288,13 @@ class App extends React.Component {
               x3: x3,
               x4: x4,
 
-              total: this.state.width1 + this.state.width3 + this.state.width4,
-              proportion1: this.state.width1 / this.state.total,
-              proportion3: this.state.width3 / this.state.total,
-              proportion4: this.state.width4 / this.state.total,
-              width1: this.state.width1 * this.state.proportion1,
-              width3: this.state.width3 * this.state.proportion3,
-              width4: this.state.width4 * this.state.proportion4,
+              //total: this.state.width1 + this.state.width3 + this.state.width4,
+              //proportion1: this.state.width1 / this.state.total,
+              //proportion3: this.state.width3 / this.state.total,
+              //proportion4: this.state.width4 / this.state.total,
+              //width1: this.state.width1 * this.state.proportion1,
+              //width3: this.state.width3 * this.state.proportion3,
+              //width4: this.state.width4 * this.state.proportion4,
             });
             //checkWidth(e.id);
           }}
@@ -212,13 +340,13 @@ class App extends React.Component {
               height3: ref.offsetHeight,
               x4: x4,
 
-              total: this.state.width1 + this.state.width2 + this.state.width4,
-              proportion1: this.state.width1 / this.state.total,
-              proportion2: this.state.width2 / this.state.total,
-              proportion4: this.state.width4 / this.state.total,
-              width1: this.state.width1 * this.state.proportion1,
-              width2: this.state.width2 * this.state.proportion2,
-              width4: this.state.width4 * this.state.proportion4,
+              //total: this.state.width1 + this.state.width2 + this.state.width4,
+              //proportion1: this.state.width1 / this.state.total,
+              //proportion2: this.state.width2 / this.state.total,
+              //proportion4: this.state.width4 / this.state.total,
+              //width1: this.state.width1 * this.state.proportion1,
+              //width2: this.state.width2 * this.state.proportion2,
+              //width4: this.state.width4 * this.state.proportion4,
             });
             //checkWidth(e.id);
           }}
@@ -262,13 +390,13 @@ class App extends React.Component {
               width4: ref.offsetWidth,
               height4: ref.offsetHeight,
 
-              total: this.state.width1 + this.state.width2 + this.state.width3,
-              proportion1: this.state.width1 / this.state.total,
-              proportion2: this.state.width2 / this.state.total,
-              proportion3: this.state.width3 / this.state.total,
-              width1: this.state.width1 * this.state.proportion2,
-              width2: this.state.width2 * this.state.proportion3,
-              width3: this.state.width3 * this.state.proportion4,
+              //total: this.state.width1 + this.state.width2 + this.state.width3,
+              //proportion1: this.state.width1 / this.state.total,
+              //proportion2: this.state.width2 / this.state.total,
+              //proportion3: this.state.width3 / this.state.total,
+              //width1: this.state.width1 * this.state.proportion2,
+              //width2: this.state.width2 * this.state.proportion3,
+              //width3: this.state.width3 * this.state.proportion4,
             });
             //checkWidth(e.id);
           }}
